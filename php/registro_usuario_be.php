5e74c40a3c7353e41aedcafd164a7fcde0866738
chavs117 <47653777+chavs117@users.noreply.github.com>
2021-03-02 12:33:53 -0600

    Update registro_usuario_be.php

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -3,29 +3,14 @@
 include 'conexion_be.php';
 
 $nombre = $_POST['nombr'];
-$apellidoP = $_POST['aplld_patrn'];
-$apellidoM = $_POST['aplld_matrn'];
 $correo = $_POST['corr'];
-$calle1 = $_POST['call1'];
-$calle2 = $_POST['call2'];
-$numeroE = $_POST['num_ext'];
-$numeroI = $_POST['num_int'];
-$codigoP = $_POST['cod_pstl'];
-$estado = $_POST['estd'];
-$ciudad = $_POST['cidd'];
-$colonia = $_POST['coln'];
-$pais = $_POST['pais'];
-$cuenta = $_POST['cuenta'];
-$celular = $_POST['celular'];
 $pass = $_POST['pass'];
 
 //Encriptado de contraseña
 $pass = hash('sha512', $pass);
 
-$query = "INSERT INTO usuarios (nombr, aplld_patrn, aplld_matrn, corr, call1, call2, num_ext, num_int, cod_pstl, 
-                                estd, cidd, coln, pais, cuenta, celular, pass) 
-        VALUES ('$nombre', '$apellidoP', '$apellidoM', '$correo', '$calle1', '$calle2', '$numeroE', '$numeroI', 
-                '$codigoP', '$estado', '$ciudad', '$colonia', '$pais', '$cuenta', '$celular', '$pass')";
+$query = "INSERT INTO usuarios (nombr, corr, pass) 
+        VALUES ('$nombre', '$correo', '$pass')";
 // Verificar que el correo no se repita en la base de datos
 $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo'");
 
